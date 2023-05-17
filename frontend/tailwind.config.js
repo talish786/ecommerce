c50c9e7e0885d01bb0495148f622ca0ac0d250f8
talish786 <jam.talish@gmail.com>
2023-05-17 20:21:40 +0500

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily:{
+      Roboto:["Roboto","sans-serif"],
+      Poppins:["Poppins","sans-serif"]
+    },
+    extend: {
+      screens:{
+        "1000px":"1050px",
+        "1110px":"1110px",
+        "800px":"800px",
+        "1300px":"1300px",
+        "400px":"400px",
+      },
+    },
+  },
+  plugins: [],
+}
